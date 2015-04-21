3303fec6c6c6ac6a2068811f2da69c7634b1ad47
James Rauhut <jamesrauhut@gmail.com>
2015-04-21 17:50:54 -0500

    Added dropdown for mode change

--- js/main.js
+++ js/main.js
@@ -1,4 +1,35 @@
 $(function(){
 
+    var openToggle = function() {
+        $( "header > nav > ul .mode-option" ).addClass("hovered");
+        $( "header > nav > ul li:nth-last-child(1)" ).css("visibility","visible");
+    };
+
+    var closeToggle = function() {
+        $( "header > nav > ul .mode-option" ).removeClass("hovered");
+        $( "header > nav > ul li:nth-last-child(1)" ).css("visibility","hidden");
+    };
+
+    //Check for touch device
+    if (!!('ontouchstart' in window)){
+        // Behaviour and events for touch device
+        $( "header > nav > ul li:nth-last-child(1)" ).toggle(
+            function() {
+                openToggle();
+            }, function() {
+                closeToggle();
+            }
+        );
+    } else {
+        //Behaviour and events for pointing device like mouse
+        $( "header > nav > ul .mode-option" ).hover(
+            function() {
+                openToggle();
+            }, function() {
+                closeToggle();
+            }
+        );
+    };
+
 })
 
