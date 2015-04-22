90724286039677b05aaea5689f85f4873437a595
James Rauhut <jamesrauhut@gmail.com>
2015-04-22 00:37:11 -0500

    Added slider for switching modes that displays selected class

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(function(){
         $( "header > nav > ul li:nth-last-child(1)" ).css("visibility","hidden");
     };
 
-    //Check for touch device
+    // Check for touch device
     if (!!('ontouchstart' in window)){
         // Behaviour and events for touch device
         $( "header > nav > ul li:nth-last-child(1)" ).toggle(
@@ -21,7 +21,7 @@ $(function(){
             }
         );
     } else {
-        //Behaviour and events for pointing device like mouse
+        // Behaviour and events for pointing device like mouse
         $( "header > nav > ul .mode-option" ).hover(
             function() {
                 openToggle();
@@ -31,5 +31,33 @@ $(function(){
         );
     };
 
+    // Slider changes
+    var modeSwitch = function() {
+        switch ($('#mode-slider').val()) {
+            case '1':
+                $('.mode-option:first > span').text("Casual User");
+                $('.casual').removeClass('display-none');
+                $('.junkie').addClass('display-none');
+                $('.pro').addClass('display-none');
+                break;
+            case '2':
+                $('.mode-option:first > span').text("Movie Junkie");
+                $('.casual').addClass('display-none');
+                $('.junkie').removeClass('display-none');
+                $('.pro').addClass('display-none');
+                break;
+            case '3':
+                $('.mode-option:first > span').text("Film Pro");
+                $('.casual').addClass('display-none');
+                $('.junkie').addClass('display-none');
+                $('.pro').removeClass('display-none');
+                break;
+        }
+    };
+
+    modeSwitch();
+    $('#mode-slider').on('input', function(){
+        modeSwitch();
+    });
 })
 
