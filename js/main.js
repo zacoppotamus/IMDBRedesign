d7818d9292ae2f753030b503c9c548a21cc9c034
Zac Ioannidis <zacoppotamus@gmail.com>
2015-05-03 11:53:06 -0500

    Added news and box office

--- js/main.js
+++ js/main.js
@@ -44,18 +44,53 @@ $(function(){
                 $('.junkie').addClass('display-none');
                 $('.pro').addClass('display-none');
                 $('.casual').removeClass('display-none');
+                resizeCarousels(1);
                 break;
             case '2':
                 $('.mode-option:first > span').text("Movie Junkie");
                 $('.casual').addClass('display-none');
                 $('.pro').addClass('display-none');
                 $('.junkie').removeClass('display-none');
+                resizeCarousels(2);
                 break;
             case '3':
                 $('.mode-option:first > span').text("Film Pro");
                 $('.casual').addClass('display-none');
                 $('.junkie').addClass('display-none');
                 $('.pro').removeClass('display-none');
+                resizeCarousels(3);
+                break;
+        }
+    };
+
+    var resizeCarousels = function() {
+        switch($('#mode-slider').val()) {
+            case '1':
+                $('.carousel-container').animate({'width': '100%'}, 500);
+                break;
+            case '2':
+                $('.carousel-container').animate({
+                    'width': '53%',
+                    'margin-right': '3%'
+                }, 500);
+                $('.news').animate({
+                    'position': 'absolute',
+                    'padding-top': '38px'
+                }, 500);
+                // $container.masonry();
+                break;
+            case '3':
+                $('.carousel-container').animate({
+                    'width': '53%',
+                    'margin-right': '3%'
+                }, 500);
+                $('.news').animate({
+                    'position': 'absolute',
+                    'padding-top': '38px'
+                }, 500);
+                $('.box-office').animate({
+                    'margin-top': '240px'
+                }, 500);
                 break;
         }
     };
@@ -67,7 +102,7 @@ $(function(){
     });
 
     $(window).resize(function(){
-        $container.masonry();  
+        $container.masonry();
     });
 })
 
